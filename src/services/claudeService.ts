031c6c3c6f6d009e6150580e6a6313eb1f610564
Warrick Smith <warrick.smith@neo.nz>
2024-01-17 09:06:38 +1300

    feat: update payload and response validators

--- src/services/claudeService.ts
+++ src/services/claudeService.ts
@@ -5,10 +5,32 @@ const anthropic = new Anthropic({
     process.env.APIKEY,
 })
 
+interface Content {
+  type: string
+  text: string
+}
+
+interface Response {
+  id: string
+  type: string
+  role: string
+  content: Content[]
+  model: string
+  stop_reason: string
+  stop_sequence: any
+}
+
+const validateResponse = (response: Response): boolean =>
+  Array.isArray(response.content) &&
+  response.content.every(
+    (contentItem) =>
+      typeof contentItem.type === 'string' &&
+      typeof contentItem.text === 'string'
+  )
+  
 const getPromptResponse = async (payload: any) => {
   let response
   let attempts = 0
-
   do {
     response = await anthropic.beta.messages.create({
       model: 'claude-2.1',
@@ -20,23 +42,22 @@ const getPromptResponse = async (payload: any) => {
         },
       ],
     })
-
+    console.log("Response : ", response)
     if (isValidResponse(response)) {
-      return response
+      return response.content[0].text || {}
     }
 
     attempts++
-  } while (attempts < 3)
+  } while (attempts < 2)
 
   throw new Error('Invalid response from Claude API after 3 attempts')
 }
 
 const isValidResponse = (response: any) => {
-  // This is a placeholder for a real validation function.
-  // You would replace this with your actual validation logic,
-  // which may include checking the structure of the response,
-  // ensuring that necessary fields are present and correctly formatted, etc.
-  return true // Assuming all responses are valid for this example.
+  const isValid = validateResponse(response)
+  console.log("Result of response validation: ",isValid)
+
+  return isValid 
 }
 
 export default { getPromptResponse }
--- src/validators/payloadValidator.ts
+++ src/validators/payloadValidator.ts
@@ -1,7 +1,4 @@
 const payloadValidator = (payload: any): boolean => {
-  // Implement your validation logic here
-  // For example, check that payload has a 'content' property of type string
-  console.log("Payload : ", payload)
   console.log('TypeOf Payload Content : ', typeof payload.prompt)
   return typeof payload.prompt === 'string'
 }
